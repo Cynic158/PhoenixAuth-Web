32c998db645a6a36f8bcb8d6bcd286d516a495dd
Cynic158 <115249055+Cynic158@users.noreply.github.com>
2024-01-14 20:18:30 +0800

    perf: add iframe listener

--- src/views/apiList/index.vue
+++ src/views/apiList/index.vue
@@ -2,7 +2,7 @@
   <div class="list-iframe-div">
     <iframe
       class="list-iframe-container"
-      ref="myIframe"
+      id="listIframe"
       src="https://liliya233.uk/openapi/swagger/index.html"
       frameborder="0"
       @load="iframeLoadedFunc"
@@ -13,20 +13,31 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import useSettingStore from "@/store/modules/setting";
+let settingStore = useSettingStore();
 
 let iframeLoaded = ref(false);
 let iframeLoadedFunc = () => {
   // iframe加载完成后触发的回调
   iframeLoaded.value = true;
+  var myIframe = document.getElementById("listIframe");
+  // @ts-ignore
+  var iframeContentWindow = myIframe.contentWindow || myIframe.contentDocument;
+  iframeContentWindow.addEventListener("click", () => {
+    if (settingStore.mmenuFlag == true && settingStore.isCollapse == false) {
+      settingStore.reqCollapse++;
+    }
+  });
 };
 </script>
 
 <style lang="scss">
-.list-iframe-div{
-  width: 100%; 
-  height: 100%; 
+.list-iframe-div {
+  width: 100%;
+  height: 100%;
   position: relative;
-  @supports (-webkit-touch-callout: none) { // 检测是否支持 -webkit-fill-available
+  @supports (-webkit-touch-callout: none) {
+    // 检测是否支持 -webkit-fill-available
     height: -webkit-fill-available; // 设置 Safari 专有的 height 属性
   }
 }
