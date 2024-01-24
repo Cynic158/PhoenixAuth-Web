ccf98a6bad9bcadd9684b47e254f56d25d50586e
Liliya233 <79683806+Liliya233@users.noreply.github.com>
2024-01-25 01:31:18 +0800

    Improve pnpm pack

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,8 @@ const getEnvFn = (mode: string, target: string) => {
   return loadEnv(mode, process.cwd())[target];
 };
 
+const dependencies = require('./package.json').dependencies;
+
 // https://vitejs.dev/config/
 export default ({ mode }) =>
   defineConfig({
@@ -27,11 +29,27 @@ export default ({ mode }) =>
       },
     },
     base: "/static",
-    // build: {
-    //   rollupOptions: {
-    //     external: ["avatar.webp", "avatar2.webp", "bg.webp", "bg_m.webp"],
-    //   },
-    // },
+    build: {
+      rollupOptions: {
+        treeshake: true,
+        output: {
+          // 根据不同的js库 拆分包，减少index.js包体积
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              // 指定需要拆分的第三方库或模块
+              const dependenciesKeys = Object.keys(dependencies);
+              const match = dependenciesKeys.find((item) => {
+                  return id.includes(item);
+              });
+              const notSplit = ['vue', 'ant-design-vue'];
+              if (match && !notSplit.includes(match)) {
+                  return match;
+              }
+            }
+          },
+        },
+      },
+    },
     plugins: [
       vue(),
       prismjsPlugin({
