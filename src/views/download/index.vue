4451f81f47f64628776fc1786086732f5f497884
CoozillaX <79683806+CoozillaX@users.noreply.github.com>
2026-07-13 16:04:37 +1000

    chore: Remove unsupported clients from download list

--- src/views/download/index.vue
+++ src/views/download/index.vue
@@ -38,23 +38,6 @@
     </el-card>
     <el-card style="margin-top: 12px" shadow="hover">
       <template #header>
-        <div class="card-header">ToolDelta</div>
-      </template>
-      <div>
-        <span style="display: inline-block">
-          点击以下链接前往 ToolDelta 仓库查看使用教程
-        </span>
-        <div style="margin-top: 16px">
-          <el-link
-            href="https://github.com/ToolDelta-Basic/ToolDelta"
-            target="_blank"
-            >https://github.com/ToolDelta-Basic/ToolDelta</el-link
-          >
-        </div>
-      </div>
-    </el-card>
-    <el-card style="margin-top: 12px" shadow="hover">
-      <template #header>
         <div class="card-header">Eulogist</div>
       </template>
       <div>
